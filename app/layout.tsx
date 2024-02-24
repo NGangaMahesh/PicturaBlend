028eeeeb1576d600d8ff82a13ad64bf806f3b7ca
NGangaMahesh <nijikalgangamahesh@gmail.com>
2024-02-24 13:47:39 +0530

    initaial commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from '@clerk/nextjs'
+import { cn } from "@/lib/utils";
+
+const IBMPlex = IBM_Plex_Sans({ subsets: ["latin"], weight: ['400', '500', '600', '700'],
+variable: '--font-ibm-plex' });
+
+export const metadata: Metadata = {
+  title: "PicturaBlend",
+  description: "PicturaBlend: Transforming Images Seamlessly - Restore, Fill, Recolor, Remove. Your Vision, Perfected",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider appearance={{
+      variables: {colorPrimary: '#624cf5'}
+    }}>
+      <html lang="en">
+      <body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>{children}</body>
+    </html>
+    </ClerkProvider>
+    
+  );
+}
